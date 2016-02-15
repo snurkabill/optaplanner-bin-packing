d2dd009f567e75bf53bc4af05e32949f44dae8ac
Jiri Vahala <jvahala@redhat.com>
2016-02-15 16:02:32 +0100

    initial commit

---
+++ src/main/java/snurkabill/net/BinPackingRunner.java
@@ -0,0 +1,10 @@
+package snurkabill.net;
+
+public class BinPackingRunner {
+
+    public static void main(String[] args) {
+
+
+    }
+
+}
---
+++ src/main/java/snurkabill/net/domain/BinPacking.java
@@ -0,0 +1,7 @@
+package snurkabill.net.domain;
+
+/**
+ * Created by jvahala on 15.2.16.
+ */
+public class BinPacking {
+}
